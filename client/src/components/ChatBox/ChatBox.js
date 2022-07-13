d0edd870f993d8d157f5f221a4104b6ba3251912
Georgi <georgichichev15@Gmail.com>
2022-07-13 14:18:08 +0300

    Create ChatBox component
    Add chatbox css

---
+++ client/src/components/ChatBox/ChatBox.js
@@ -0,0 +1,16 @@
+import './ChatBox.css'
+
+export const ChatBox = () => {
+    return (
+        <div
+            style={{
+                padding: 24,
+            }}
+        >
+            <ul className='chat'>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+            </ul>
+        </div>
+    )
+}
