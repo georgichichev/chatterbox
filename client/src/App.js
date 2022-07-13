c92222b60e1a72be212b824139a1ac59ddc4419b
Georgi <georgichichev15@Gmail.com>
2022-07-13 14:18:45 +0300

    Add basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
+import "antd/dist/antd.css";
+import {Layout} from 'antd';
+import {useState} from "react";
+import {SideNavbar} from "./components/SideNavbar/SideNavbar.js";
+import {ChatBox} from "./components/ChatBox/ChatBox.js";
+import {MessageForm} from "./components/MessageForm/MessageForm.js";
+
 function App() {
     return (
-        <div>
-        </div>
-    );
+        <>
+            <Layout style={{
+                minHeight: '100vh',
+            }} hasSider>
+                <SideNavbar/>
+                <Layout
+                    className="site-layout"
+                >
+                    <div
+                        style={{
+                            margin: '24px 16px 0',
+                        }}
+                    >
+                        <ChatBox/>
+                    </div>
+                <MessageForm/>
+                </Layout>
+            </Layout>
+        </>
+    )
 }
 
 export default App;
