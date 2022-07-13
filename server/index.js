731c79eeed500f3d1d95db31b9332af4786dc725
Georgi <georgichichev15@Gmail.com>
2022-07-13 21:13:29 +0300

    Setup database

--- server/index.js
+++ server/index.js
@@ -1,31 +1,43 @@
 const express = require('express');
+const {mongoose} = require('mongoose');
+const {Server} = require("socket.io");
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const cors = require('cors');
+const {DB_URI, CORS_OPTIONS} = require("./src/constants.js");
 
-const corsOptions = {
-    origin: 'http://localhost:3001',
-    methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS', 'HEAD'],
-    allowedHeaders: ['Content-Type', 'X-Authorization']
-}
-
-const { Server } = require("socket.io");
-const io = new Server(server,{
-    cors:{
-        origin: 'http://localhost:3001',
-        methods: ['GET', 'POST'],
+const startServer = () => {
+    try {
+        mongoose.connect(DB_URI, () => {
+            console.log('Database connected.')
+        })
+    } catch (err) {
+        console.log('Error connecting to the database.');
+        return process.exit(1);
     }
-});
 
-app.use(cors(corsOptions));
+    app.use(cors(CORS_OPTIONS));
 
-io.on('connection', (socket) =>{
-    socket.on('send message', message =>{
-        io.emit('send message', message);
+    const io = new Server(server, {
+        cors: {
+            origin: 'http://localhost:3001',
+            methods: ['GET', 'POST'],
+        }
+    });
+
+
+    io.on('connection', (socket) => {
+        socket.on('send message', message => {
+            io.emit('send message', message);
+        })
     })
-})
 
-server.listen(3000, () => {
-    console.log('Server listening on port 3000...');
-});
+    server.listen(3000, () => {
+        console.log('Server listening on port 3000...');
+    });
+};
+
+startServer();
+
+
