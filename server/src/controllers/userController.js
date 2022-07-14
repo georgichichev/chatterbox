af482bfc3bd7788c7ab0c77b07a7dbc1fa77a148
Georgi <georgichichev15@Gmail.com>
2022-07-14 17:35:04 +0300

    Add user controller

---
+++ server/src/controllers/userController.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const userService = require('../service/userService.js');
+const {errorMapper} = require('../util.js');
+
+router.post('/register',async (req, res) => {
+    try{
+        const user = await userService.register(req.body);
+
+        res.json(user);
+    }
+    catch (err){
+        const message = errorMapper(err);
+        res.status(400).json({message});
+    }
+});
+
+router.post('/login',async (req, res) => {
+    try {
+        const user = await userService.login(req.body);
+
+        res.json(user);
+    }
+    catch (err){
+        const message = errorMapper(err);
+
+        res.status(400).json({message});
+    }
+});
+
+router.get('/logout', (req, res) => {
+    userService.logout(req.token);
+
+    res.status(204).end();
+})
+
+module.exports = router;
