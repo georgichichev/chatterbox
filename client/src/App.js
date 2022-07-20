040d857db05d25cf4bd81e86e82c90bc0a2b5b83
Georgi <georgichichev15@Gmail.com>
2022-07-20 12:11:20 +0300

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,20 @@ import {Layout} from 'antd';
 import {useState} from "react";
 import {SideNavbar} from "./components/SideNavbar/SideNavbar.js";
 import {ChatBox} from "./components/ChatBox/ChatBox.js";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Rooms from "./components/Rooms/Rooms.js";
 
 function App() {
     return (
-        <>
-            <Layout style={{minHeight: '100vh'}} hasSider>
-                <SideNavbar/>
-                <Layout className="site-layout">
-                    <div style={{margin: '24px 16px 0'}}>
-                        <ChatBox/>
-                    </div>
-                </Layout>
+        <Layout style={{minHeight: '100vh'}} hasSider>
+            <SideNavbar/>
+            <Layout className="site-layout">
+                <Routes>
+                    <Route path='/chat' element={<ChatBox/>}/>
+                    <Route path='/rooms' element={<Rooms/>}/>
+                </Routes>
             </Layout>
-        </>
+        </Layout>
     )
 }
 
--- client/src/components/SideNavbar/SideNavbar.js
+++ client/src/components/SideNavbar/SideNavbar.js
@@ -2,22 +2,40 @@ import {Menu} from "antd";
 import Sider from "antd/es/layout/Sider.js";
 import {useState} from "react";
 import {UsergroupAddOutlined} from "@ant-design/icons";
-
-const items = [
-    {label: 'Rooms', key: 'item-1', icon: <UsergroupAddOutlined />},
-    {label: 'Create Room', key: 'item-2'},
-    {label: 'Private Message', key: 'item-3'},
-
-];
+import {Link} from "react-router-dom";
 
 export const SideNavbar = () => {
     const [collapsed, setCollapsed] = useState(true);
 
     return (
         <Sider breakpoint={"lg"} onBreakpoint={() => setCollapsed(true)} collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-            <div className="logo"/>
-            <Menu style={{fontSize: '17px'}} theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}/>
-            <div className="logo"/>
+            <Menu style={{fontSize: '17px'}}
+                  theme="dark"
+                  defaultSelectedKeys={['1']}
+                  mode="inline"
+            >
+                <Menu.Item key="Rooms">
+                    <Link to='rooms'>
+                        Rooms
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key="Chat">
+                    <Link to='/chat'>
+                        Chat
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key="Login">
+                    <Link to='/login'>
+                        Login
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key="Register">
+                    <Link to='/register'>
+                        Register
+                    </Link>
+                </Menu.Item>
+
+            </Menu>
         </Sider>
     )
 }
